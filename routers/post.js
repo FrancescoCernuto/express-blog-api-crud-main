f2fca72b3e07082bd20196e155936a643e27ca4c
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-07 23:06:18 +0100

    add require

--- routers/post.js
+++ routers/post.js
@@ -1,19 +1,2 @@
-const express = require("express")
-const router = express.Router();
-
-module.exports = router;
-
 const express = require("express");
-const router = express.Router();
-
-const postController = require("../controllers/postController.js");
-
-// Index
-router.get("/", postController.index);
-router.get("/:id", postController.show);
-router.post("/", postController.create);
-router.put("/:id", postController.update);
-router.patch("/:id", postController.modify);
-router.delete("/:id", postController.destroy);
-
-module.exports = router
+const router = express.Router()
