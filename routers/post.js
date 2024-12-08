d9f69f6b7bd2aca31c1619102807575f2504de0f
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-08 16:04:40 +0100

    put update

--- routers/post.js
+++ routers/post.js
@@ -8,3 +8,4 @@ const postController = require("../controllers/postController.js");
 router.get("/", postController.index);
 router.get("/:id", postController.show);
 router.post("/", postController.create);
+router.put("/:id", postController.update);
